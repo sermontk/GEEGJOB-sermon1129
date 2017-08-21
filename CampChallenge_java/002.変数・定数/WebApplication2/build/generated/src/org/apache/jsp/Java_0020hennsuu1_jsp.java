d28081404b2dedbf2045c163b3e6b31a0fe1348a
SERMON\t.k <sermon1129.so@gmail.com>
2017-08-21 10:37:39 +0900

    確認お願いします。

---
+++ CampChallenge_java/002.変数・定数/WebApplication2/build/generated/src/org/apache/jsp/Java_0020hennsuu1_jsp.java
@@ -0,0 +1,79 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class Java_0020hennsuu1_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+
+    String name = "隈　貴司";
+    int age =26;
+    out.print(name+"です。<br>");
+    out.print ("年齢は"+age+"歳です");
+
+      out.write('\n');
+      out.write('\n');
+
+    int nennrei =100;
+    String namae ="soeda";
+    
+    out.print("こんにちは");
+    
+    float point =0.1f;
+    float ap = nennrei*point;
+    out.print(ap);
+    
+
+
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ CampChallenge_java/002.変数・定数/WebApplication2/build/web/Java hennsuu1.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : Java hennsuu1
+    Created on : 2017/08/19, 18:47:51
+    Author     : t.k
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%
+    String name = "隈　貴司";
+    int age =26;
+    out.print(name+"です。<br>");
+    out.print ("年齢は"+age+"歳です");
+%>
+
+<%
+    int nennrei =100;
+    String namae ="soeda";
+    
+    out.print("こんにちは");
+    
+    float point =0.1f;
+    float ap = nennrei*point;
+    out.print(ap);
+    
+
+%>
