69c42ef96fa5b1b1c8317e2838556272cb9e5226
SERMON\t.k <sermon1129.so@gmail.com>
2017-09-08 18:23:04 +0900

    Java 標準 クラスタイムスタンプの作成課題を提出します。

---
+++ CampChallenge_java/015.標準クラス/Javahyoujyunclasskkadai1/src/java/Javahyoujyunclasskkadai1/Javahyoujyunclasskkadai1.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Javahyoujyunclasskkadai1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author t.k
+ */
+public class Javahyoujyunclasskkadai1 extends HttpServlet {
+
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ParseException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            
+          
+           Calendar hyouji = Calendar.getInstance();
+                hyouji.set(2016, 0, 1 ,0, 0, 0);
+                out.print(hyouji.get(Calendar.YEAR));
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(Javahyoujyunclasskkadai1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(Javahyoujyunclasskkadai1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
