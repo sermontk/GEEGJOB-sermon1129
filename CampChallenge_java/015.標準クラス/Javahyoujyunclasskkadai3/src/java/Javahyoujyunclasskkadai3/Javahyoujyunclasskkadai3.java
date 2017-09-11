13e73670f64e7e6586f2840fae5e9f67cd47004f
SERMON\t.k <sermon1129.so@gmail.com>
2017-09-11 18:26:30 +0900

    Java標準クラス　タイムスタンプの表示1　課題を提出します。　

---
+++ CampChallenge_java/015.標準クラス/Javahyoujyunclasskkadai3/src/java/Javahyoujyunclasskkadai3/Javahyoujyunclasskkadai3.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Javahyoujyunclasskkadai3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * 
+ *getTimeInMillis
+ * @author t.k
+ */
+public class Javahyoujyunclasskkadai3 extends HttpServlet {
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            Calendar hiduke = Calendar.getInstance();
+                hiduke.set(2016, 10, 4 ,10, 0, 0);
+          
+            
+            SimpleDateFormat hyouji = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            out.print(hyouji.format(hiduke.getTime()));
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
