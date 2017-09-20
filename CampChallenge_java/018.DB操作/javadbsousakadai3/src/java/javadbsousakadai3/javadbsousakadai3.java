c4595a14e635f16a94659e078d356bb50d5cc689
SERMON\t.k <sermon1129.so@gmail.com>
2017-09-20 18:33:04 +0900

    JavaDB操作　全情報の取得と表示　課題を提出します。

---
+++ CampChallenge_java/018.DB操作/javadbsousakadai3/src/java/javadbsousakadai3/javadbsousakadai3.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadbsousakadai3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+/**
+ *
+ * @author t.k
+ */
+public class javadbsousakadai3 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {  
+            
+            
+            Connection conn = null;
+            PreparedStatement ps = null;
+            ResultSet rd = null;
+            
+           
+            try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db","sermon1129","tk07041129");
+            out.print("データベース接続完了"+"<br>");
+            
+            ps = conn.prepareStatement("select * from profiles");
+            rd = ps.executeQuery();
+            while(rd.next()){
+                out.print("ID;"+rd.getInt("profilesID")+"<br>");
+                out.print("名前;"+rd.getString("name")+"<br>");
+                out.print("電話番号;"+rd.getString("tel")+"<br>");
+                out.print("年齢;"+rd.getInt("age")+"<br>");
+                out.print("誕生日;"+rd.getString("birthday")+"<br>");
+            }
+            
+            
+            }catch (SQLException e) {
+                out.print(e.getMessage());
+            } catch (Exception e) {
+                out.print(e.getMessage());
+            } finally {
+                if (conn != null) {
+                    try {
+
+                        conn.close();
+                        out.print("DBとの切断に成功");
+
+                    } catch (Exception e) {
+
+                        out.print(e.getMessage());
+                    }
+
+                }
+
+            }
+    }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
